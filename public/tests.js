232a0c31697643894e3c599bd1d38350f4dc1d42
Jorge Alonso <alu0100767803@ull.edu.es>
2017-04-23 16:28:19 +0100

    Empezando las pruebas

--- public/tests.js
+++ public/tests.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
   var assert;
 
   assert = chai.assert;
@@ -15,13 +15,43 @@
     test('Multiplications are parsed correctly', () => {
       var result = parse('4*2');
       console.log(result);
-      assert.deepEqual(result, {type: "*", 
-                                left: { type: "NUM", value: 4}, 
+      assert.deepEqual(result, {type: "*",
+                                left: { type: "NUM", value: 4},
                                 right: {type: "NUM", value: 2}
       });
     });
+    test('Probando asignación', () => {
+      var result = parse('a = 5');
+      console.log(result);
+      assert.deepEqual(result, {type: "=",
+                                left: "a",
+                                right: {type: "NUM", value: 5}
+      });
+    });
     test('Bad expressions throw exceptions', () => {
-      assert.throws(() => parse('3 + (4+2))'), /Syntax\s+Error/i);
+      assert.throws(() => parse('4 + 2'), /Syntax\s+Error/i);
     });
   });
+}).call(this);*/
+
+(function() {
+  var assert;
+  var expect;
+  assert = chai.assert;
+  expect = chai.expect;
+
+  suite('parser', function() {
+    setup(function() {
+    });
+
+    test('Probando que el parser devueva un objeto', () => {
+      var result = parse('var a; a = 5');
+      console.log(result);
+      expect(result).to.be.an('Object');
+    });
+
+  
+
+  });
+
 }).call(this);
